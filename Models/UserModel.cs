4a9099f056cfdeb0b80e2f3447b91da1b40182e0
Charmaine Connors <charmainez.83@googlemail.com>
2020-07-07 15:26:24 +0100

    Updated with Interface.
    Project folder added to the app 'MovieRepositoryService'. The project
    folder includes two files 'ServiceRepository' and 'IMovieRepository'.
    The Interface has been attached to the Quaranteam APP. Also
    NewtonSoft.Json has been installed.

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,18 +1,18 @@
-﻿using System;
-namespace Quaranteam_APP.Models
-{
-    public class UserModel
-    {
+﻿//using System;
+//namespace Quaranteam_APP.Models
+//{
+//    public class UserModel
+//    {
    
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Email { get; set; }
-            public string UserName { get; set; }
-            public string PassWord { get; set; }
-            public string ConfirmPassword { get; set; }
-            public int Age { get; set; }
+//            public string FirstName { get; set; }
+//            public string LastName { get; set; }
+//            public string Email { get; set; }
+//            public string UserName { get; set; }
+//            public string PassWord { get; set; }
+//            public string ConfirmPassword { get; set; }
+//            public int Age { get; set; }
         
-    }
+//    }
 
-}
+//}
 
