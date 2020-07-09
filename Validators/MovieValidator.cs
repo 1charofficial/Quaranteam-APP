f9a68c3e6a11008620161dbd6f26aeba3bb59ead
Charmaine Connors <charmainez.83@googlemail.com>
2020-07-09 14:52:42 +0100

    Updated the project file with Interface. Added project: MoveiRepositoryService. Added files IMovieRepository and ServieRepository. Installed NewtonSoft.Json. Partial Fluent Validation files - still in progress.

--- Validators/MovieValidator.cs
+++ Validators/MovieValidator.cs
@@ -4,27 +4,28 @@ using DapperCRUDAPI.Models;
 using Quaranteam_APP.Models;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace Quaranteam_APP.Validators
 {
     public class MovieValidator : AbstractValidator<Movie>
     {
 
 
-        public MovieValidator()
-        {
-            RuleFor(m => m.MovieID).NotEmpty().GreaterThan(0);
+        //public MovieValidator()
+        //{
+        //    RuleFor(m => m.MovieID).NotEmpty().GreaterThan(0);
 
-            MovieValidator validator = new MovieValidator();
+        //    MovieValidator validator = new MovieValidator();
 
-            ValidationResult results = validator.Validate(MovieID);
+        //    ValidationResult results = validator.Validate(Movie);
 
-            if(results.Equals == false)
-            {
+        //    if(results.Equals == false)
+        //    {
 
-            }
+        //    }
 
 
-        }
+        //}
 
         private ValidationResult Validate(object movieRepository)
         {
